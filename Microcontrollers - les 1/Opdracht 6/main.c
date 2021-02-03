0ad7335a5bf70cee641ddeadb66e50056e00cbe4
Thaomas <thaomas35@gmail.com>
2021-02-03 12:16:07 +0100

    Opdracht 6 done

---
+++ Microcontrollers - les 1/Opdracht 6/main.c
@@ -0,0 +1,55 @@
+/*
+ * Opdracht 6.c
+ *
+ * Created: 3-2-2021 11:36:05
+ * Author : Thaomas
+ */ 
+
+#define F_CPU 8e6
+#include <avr/io.h>
+#include <util/delay.h>
+
+void wait(int ms);
+void toggle();
+
+int dur;
+int bool;
+
+int main(void)
+{
+    DDRD = 0b11111110;
+    PORTC = 0x01;
+	dur = 500;
+	bool = 0;
+    while (1) 
+    {
+		PORTD = 0x80;
+		wait(dur);
+		
+		PORTD = 0x00;
+		wait(dur);	
+    }
+}
+
+void wait( int ms ){
+	for (int i=0; i<ms; i++) {
+		_delay_ms( 1 );
+		
+		if (PINC & 0x01){
+			toggle();
+			return;
+		}
+		
+	}
+}
+
+void toggle(){
+	if (bool)
+	{
+		dur = 500;
+		bool = 0;
+	}else{
+		dur = 125;
+		bool = 1;
+	}
+}
