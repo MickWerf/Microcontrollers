8d7ae403938b854bc41ba3f0fcdc3d173a7a2dea
Thaomas <thaomas35@gmail.com>
2021-02-03 10:49:14 +0100

    Opdracht 2

--- Microcontrollers - les 1/Opdracht 2/main.c
+++ Microcontrollers - les 1/Opdracht 2/main.c
@@ -1,18 +1,33 @@
 /*
  * Opdracht 2.c
  *
- * Created: 03/02/2021 10:47:16
+ * Created: 03/02/2021 09:22:15
  * Author : Mick
  */ 
 
+#define F_CPU 8e6
+
 #include <avr/io.h>
+#include <util/delay.h>
 
 
 int main(void)
 {
-    /* Replace with your application code */
+	DDRD = 0b11111111;			// All pins PORTD are set to output 
+    
     while (1) 
     {
+		PORTD = 0x80;
+		wait (500);
+		PORTD = 0x40;
+		wait (500);
     }
+}
+
+void wait( int ms )
+{
+	for (int i=0; i<ms; i++) {
+		_delay_ms( 1 );
+	}
 }
 
