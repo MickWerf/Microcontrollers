18785920f670738072db42b0b921367d4401ba1e
MickWerf <mickwerf@outlook.com>
2021-02-03 10:30:46 +0100

    opdracht 1

--- Microcontrollers - les 1/Opdracht 1/main.c
+++ Microcontrollers - les 1/Opdracht 1/main.c
@@ -5,14 +5,29 @@
  * Author : Mick
  */ 
 
+#define F_CPU 8e6
+
 #include <avr/io.h>
+#include <util/delay.h>
 
 
 int main(void)
 {
-    /* Replace with your application code */
+	DDRD = 0b11111111;			// All pins PORTD are set to output 
+    
     while (1) 
     {
+		PORTD = 0x80;
+		wait (500);
+		PORTD = 0x40;
+		wait (500);
     }
+}
+
+void wait( int ms )
+{
+	for (int i=0; i<ms; i++) {
+		_delay_ms( 1 );
+	}
 }
 
