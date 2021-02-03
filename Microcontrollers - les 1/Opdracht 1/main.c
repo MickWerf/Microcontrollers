c0eef798955d0d35e5b34132772f54f8e8858b75
MickWerf <55532589+MickWerf@users.noreply.github.com>
2021-02-03 09:24:19 +0100

    Added inital project files.

---
+++ Microcontrollers - les 1/Opdracht 1/main.c
@@ -0,0 +1,18 @@
+/*
+ * Opdracht 1.c
+ *
+ * Created: 03/02/2021 09:22:15
+ * Author : Mick
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
