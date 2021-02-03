4febd4661d442f2afba51fb48d6442e16653527e
MickWerf <mickwerf@outlook.com>
2021-02-03 10:39:16 +0100

    Added opdracht 3

---
+++ Microcontrollers - les 1/Opdracht 3/Opdracht3.c
@@ -0,0 +1,18 @@
+/*
+ * Opdracht 3.c
+ *
+ * Created: 03/02/2021 10:38:48
+ * Author : Mick
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
