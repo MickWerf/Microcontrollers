198d10971783c141e7570c0b59d2c5d7ca5dfb17
MickWerf <mickwerf@outlook.com>
2021-02-10 13:47:21 +0100

    Inital setup for opdracht 5

--- Microcontrollers - les 2/Opdracht 5/LCD.c
+++ Microcontrollers - les 2/Opdracht 5/LCD.c
@@ -4,3 +4,25 @@
  * Created: 10/02/2021 13:09:42
  *  Author: Mick
  */ 
+
+#define F_CPU 8e6
+
+#include "LCD.h"
+#include <avr/io.h>
+#include <util/delay.h>
+#include <avr/interrupt.h>
+
+void init() // Initializes the LCD Screen.
+{
+	
+}
+		 
+void display_text(char *str) // Displays a text on the LCD Screen.
+{
+	
+}
+	 
+void set_cursor(int position) // Sets the cursor on the LCD Screen.
+{
+
+} 
