c0b1c0b393d648b2b3ccc2dfd9a8204c31f90e86
MickWerf <mickwerf@outlook.com>
2021-02-10 12:34:47 +0100

    Opdracht 4, I dun the thing.

---
+++ Microcontrollers - les 2/Opdracht 4/main.c
@@ -0,0 +1,36 @@
+/*
+ * Opdracht 4.c
+ *
+ * Created: 10/02/2021 12:24:50
+ * Author : Mick
+ */ 
+
+#define F_CPU 8e6
+#include <avr/io.h>
+#include <util/delay.h>
+#include <avr/interrupt.h>
+
+
+int main(void)
+{
+	DDRD = 0xFF; // sets all PORTD Leds to output.
+	int animationSize = 16;
+	int delay = 500;
+	char pattern[] = {0x3F, 0x06, 0x5B, 0x4F, 0x66, 0x6D, 0x7D, 0x07, 0x7F, 0x6F, 0x77, 0x7C, 0x39, 0x5E, 0x79, 0x71};
+		
+    while (1) 
+    {
+		for (int i = 0; i < animationSize; i++)
+		{
+			PORTD = pattern[i];
+			wait(delay);
+		}
+    }
+}
+
+void wait( int ms ) {
+	for (int i=0; i<ms; i++) {
+		_delay_ms( 1 );
+	}
+}
+
