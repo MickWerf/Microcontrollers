d958c0f39859619449719bec02482c8c7ae5ebaf
MickWerf <mickwerf@outlook.com>
2021-02-10 11:20:00 +0100

    Completed opdracht 2

--- Microcontrollers - les 2/Opdracht 2/main.c
+++ Microcontrollers - les 2/Opdracht 2/main.c
@@ -5,14 +5,43 @@
  * Author : Mick
  */ 
 
+#define F_CPU 8e6
 #include <avr/io.h>
-
+#include <util/delay.h>
+#include <avr/interrupt.h>
 
 int main(void)
 {
-    /* Replace with your application code */
+    DDRD = 0xFF; // sets all PORTD Leds to output.
+	DDRE = 0x00; // sets all PORTE Leds as input.
+	PORTD = 0x01; // sets the first PORTD led (PD0) on.
+	
+	EICRB |= 0x0F; // Configures the INT4 and INT5 buttons as rising edge. (EICRB applies to INT 4 through 7).
+	EIMSK |= 0x30; // Enables the INT4 and INT5 interrupts.
+	sei(); // Turns all interrupts on.
+	
     while (1) 
     {
     }
+}
+
+// Interrupt using the PE4 button.
+ISR(INT4_vect ) {
+	if (PORTD == 0x80) //Checks if led is at an edge, if so wrap.
+	{
+		PORTD = 0x01;
+	} else {
+		PORTD = PORTD << 1;
+	}
+}
+
+// Interrupt using the PE5 button.
+ISR (INT5_vect) {
+	if (PORTD == 0x01) //Checks if led is at an edge, if so wrap.
+	{
+		PORTD = 0x80;
+		} else {
+		PORTD = PORTD >> 1;
+	}
 }
 
