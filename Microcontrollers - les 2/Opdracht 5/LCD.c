d345f2f39d67a311af901f03ec3fd9220217a100
Thaomas <thaomas35@gmail.com>
2021-02-11 15:22:01 +0100

    Created opdr 5

--- Microcontrollers - les 2/Opdracht 5/LCD.c
+++ Microcontrollers - les 2/Opdracht 5/LCD.c
@@ -12,9 +12,42 @@
 #include <util/delay.h>
 #include <avr/interrupt.h>
 
+#define LCD_E 	3
+#define LCD_RS	2
+
+void lcd_strobe_lcd_e(void) {
+	PORTC |= (1<<LCD_E);	// E high
+	_delay_ms(1);			// nodig
+	PORTC &= ~(1<<LCD_E);  	// E low
+	_delay_ms(1);			// nodig?
+}
+
 void init() // Initializes the LCD Screen.
 {
-	
+	DDRC = 0xFF;
+	PORTC = 0x00;
+
+	// Step 2 (table 12)
+	PORTC = 0x20;
+	lcd_strobe_lcd_e();
+
+	// Step 3 (table 12)
+	PORTC = 0x20;
+	lcd_strobe_lcd_e();
+	PORTC = 0x80;
+	lcd_strobe_lcd_e();
+
+	// Step 4 (table 12)
+	PORTC = 0x00;
+	lcd_strobe_lcd_e();
+	PORTC = 0xF0;
+	lcd_strobe_lcd_e();
+
+	// Step 4 (table 12)
+	PORTC = 0x00;
+	lcd_strobe_lcd_e();
+	PORTC = 0x60;
+	lcd_strobe_lcd_e();
 }
 		 
 void display_text(char *str) // Displays a text on the LCD Screen.
