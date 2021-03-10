6f61a561028b950241da8f77fe0a17816752e0b7
MickWerf <mickwerf@outlook.com>
2021-03-10 09:27:01 +0100

    Added les 4-5 setup

---
+++ Microcontrollers - les 4-5/Opdracht 1/main.c
@@ -0,0 +1,18 @@
+/*
+ * Opdracht 1.c
+ *
+ * Created: 10/03/2021 09:26:38
+ * Author : Mick
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
