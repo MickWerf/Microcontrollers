d11bb6f23b7b2f8f9f412f8832c5772b06047aa8
MickWerf <mickwerf@outlook.com>
2021-03-17 09:40:29 +0100

    LCD code rework

--- Microcontrollers - les 2/Opdracht 5/LCD.c
+++ Microcontrollers - les 2/Opdracht 5/LCD.c
@@ -32,32 +32,28 @@ void init() // Initializes the LCD Screen.
 	DDRC = 0xFF;
 	PORTC = 0x00;
 
-	// turn display off
-	write_command(0x00);
-	lcd_strobe_lcd_e();
-
 	// clear display
-	write_command(0x01);
+	PORTC(0x01);
 	lcd_strobe_lcd_e();
 	
 	// Return Home
-	write_command(0x02);
+	PORTC(0x02);
 	lcd_strobe_lcd_e();
 
 	//set function: 4 bits interface data, 2 lines, 5x8 dots
-	write_command(0x28);
+	PORTC(0x28);
 	lcd_strobe_lcd_e();
 	
 	// display: on, cursor on, blinking off
-	write_command(0x0E);
+	PORTC(0x0E);
 	lcd_strobe_lcd_e();
 	
 	// entry mode: cursor to right, no shift 
-	write_command(0x06);
+	PORTC(0x06);
 	lcd_strobe_lcd_e();
 	
 	// RAM address: 0, first position, line 1 
-	write_command(0x80);
+	PORTC(0x80);
 	lcd_strobe_lcd_e();
 	
 }
@@ -71,26 +67,26 @@ void display_text(char *str) // Displays a text on the LCD Screen.
 	 
 void set_cursor(int position) // Sets the cursor on the LCD Screen.
 {
-
+	
 }
 
 void write_char(char character) {
-	PORTC = character & 0xF0; // hoge nibble (first chunk)
+	PORTC = character // hoge nibble (first chunk)
 	PORTC |= 0x0C; // start writing character.
 	lcd_strobe_lcd_e();
 	
-	PORTC = ((character & 0x0F) << 4); // lage nibble (last chunk)
+	PORTC = character << 4; // lage nibble (last chunk)
 	PORTC |= 0x0C; // start writing character.
 	lcd_strobe_lcd_e();
 }
 
 void write_command(char command) {
-	PORTC = command & 0xF0; // hoge nibble (first chunk)
-	PORTC |= 0x08; // start writing character.
+	PORTC = command // hoge nibble (first chunk)
+	PORTC |= 0x08; // start writing command.
 	lcd_strobe_lcd_e();
 	
-	PORTC = ((command & 0x0F) << 4); // lage nibble (last chunk)
-	PORTC |= 0x08; // start writing character.
+	PORTC = command << 4; // lage nibble (last chunk)
+	PORTC |= 0x08; // start writing command.
 	lcd_strobe_lcd_e();
 }
 
