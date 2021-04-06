6410bb9f6e64dd3040acb4883ff455a182b0ba63
MickWerf <mickwerf@outlook.com>
2021-04-06 10:25:31 +0200

    Added interrupts

--- Eindproject/Eindproject/Eindproject/main.c
+++ Eindproject/Eindproject/Eindproject/main.c
@@ -7,14 +7,34 @@
 
 #include <avr/io.h>
 
+int turnTo = 0;
 
 int main(void)
 {
-    /* Replace with your application code */
+	EICRB |= 0x0F; // Configures the INT4 and INT5 buttons as rising edge. (EICRB applies to INT 4 through 7).
+	EIMSK |= 0x30; // Enables the INT4 and INT5 interrupts.
+	sei(); // Turns all interrupts on.
+	
     while (1) 
     {
-		
-		
+		if (turnTo == 1)
+		{
+			//turn left
+		} else if (turnTo == 2)
+		{
+			//turn right
+		}
+		wait(1);
     }
+}
+
+// Interrupt using the PE4 button.
+ISR(INT4_vect ) {
+	turnTo = 1; //turn to left
+}
+
+// Interrupt using the PE5 button.
+ISR (INT5_vect) {
+	turnTo = 2; //turn to right
 }
 
